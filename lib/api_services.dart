a626a08b096e606dedf4d50b7c6cebb7582f9901
sumitkhunt <sumitkhunt14@gmail.com>
2025-01-16 13:59:24 +0530

    GET api integretion with ui success

--- lib/api_services.dart
+++ lib/api_services.dart
@@ -1,4 +1,3 @@
-import 'dart:convert'; // For JSON decoding
 import 'package:http/http.dart' as http;
 
 class ApiServices {
@@ -8,19 +7,12 @@ class ApiServices {
     Map<String, String> requestHeaders = {'Content-Type': 'application/json'};
     var url = Uri.https("reqres.in", "/api/users");
 
-    try {
-      var response = await client.get(url, headers: requestHeaders);
+    var response = await client.get(url, headers: requestHeaders);
 
-      if (response.statusCode == 200) {
-        // Parse and return the JSON response
-        return jsonDecode(response.body);
-      } else {
-        // Handle non-200 status codes
-        return {'error': 'Failed to fetch data', 'status': response.statusCode};
-      }
-    } catch (e) {
-      // Handle exceptions (e.g., network issues)
-      return {'error': 'An exception occurred', 'details': e.toString()};
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      return {'error': 'Failed to fetch data', 'status': response.statusCode};
     }
   }
 }
